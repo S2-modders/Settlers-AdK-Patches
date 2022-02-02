ad10b595ce7c4602f98d66633de26a637c55b56a
zocker-160 <zocker1600@posteo.net>
2022-02-02 21:11:16 +0100

    initial version

---
+++ src/ZoomPatch.h
@@ -0,0 +1,32 @@
+/*
+ * Widescreen patch for Die Siedler: Aufbruch der Kulturen by zocker_160
+ *
+ * This source code is licensed under GPL-v2
+ *
+ */
+
+#pragma once
+#include <Windows.h>
+
+#define URLMAX 100
+
+struct memoryPTR {
+    DWORD base_address;
+    int total_offsets;
+    int offsets[];
+};
+
+struct threadData {
+    bool bDebugMode;
+    bool bWideView;
+    float ZoomIncrement;
+    bool bBannerPatch;
+    char BannerURL_1[URLMAX]; // used in main menu
+    char BannerURL_2[URLMAX]; // used in lobby screen
+    char BannerURL_3[URLMAX]; // unknown
+};
+
+const int version_maj = 1;
+const int version_min = 4;
+
+DWORD WINAPI ZoomPatchThread(LPVOID param);
